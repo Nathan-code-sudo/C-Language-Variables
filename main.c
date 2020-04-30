7d0a684ba4e71897764a20fa0a6400812c644b50
Nathaniel <64310904+Nathan-code-sudo@users.noreply.github.com>
2020-04-30 22:12:55 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName [] = "Nibba";
+    int characterAge = 67;
+
+    printf("your name is %s\n", characterName );
+    printf("your age is %d years old. \n", characterAge );
+    return 0;
+}
